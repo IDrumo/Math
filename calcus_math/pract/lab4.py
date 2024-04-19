deba291d614cacf17c0a802d26d7fecdcb5dae00
Drumo <streltcov.id@students.dvfu.ru>
2024-04-19 12:33:32 +1000

    Функция для проверки точности

--- calcus_math/pract/lab4.py
+++ calcus_math/pract/lab4.py
@@ -1,3 +1,15 @@
 from support.subs import newton_cotes_coefficient, get_all_NC_coefficient
 
 print(get_all_NC_coefficient(1))
+
+
+def main():
+    # Определение переменной и функции
+    x = symbols('x')
+    f = (x - 1) ** 2 - exp(-x)
+    a, b = 1.0, 1.5
+    n = 10
+    h = (b - a) / n
+
+if __name__ == "__main__":
+    main()
