2cad696971f19488760ea8fe0545983844d5eef0
Drumo <streltcov.id@students.dvfu.ru>
2024-12-03 10:28:45 +1000

    Наработка лабы 11

--- calcus_math/course 3/lab11.py
+++ calcus_math/course 3/lab11.py
@@ -1,7 +1,10 @@
 import numpy as np
 
 
-def rotation_method(A: np.array, p: int = 5) -> np.array:
+def rotation_method(A: np.array, p: int = 10) -> np.array:
+    # По умолчанию ставлю p=10 - количество преград.
+    # Чем больше преград, тем лучше сходимость и точность метода, но больше вычислений,
+    # а следовательно и более высокое время выполнения.
     def sgn(value):
         return 1 if value >= 0 else -1
 
@@ -28,12 +31,12 @@ def rotation_method(A: np.array, p: int = 5) -> np.array:
                     C[j, k] = C[k, j]
 
         C[i, i] = (c(i, j) ** 2) * A[i, i] + \
-                   2 * c(i, j) * s(i, j) * A[i, j] + \
-                   (s(i, j) ** 2) * A[j, j]
+                  2 * c(i, j) * s(i, j) * A[i, j] + \
+                  (s(i, j) ** 2) * A[j, j]
 
         C[j, j] = (s(i, j) ** 2) * A[i, i] - \
-                   2 * c(i, j) * s(i, j) * A[i, j] + \
-                   (c(i, j) ** 2) * A[j, j]
+                  2 * c(i, j) * s(i, j) * A[i, j] + \
+                  (c(i, j) ** 2) * A[j, j]
 
         C[i, j] = 0
         C[j, i] = 0
@@ -49,35 +52,39 @@ def rotation_method(A: np.array, p: int = 5) -> np.array:
                     indexes[0], indexes[1] = i, j
         return indexes[0], indexes[1]
 
-    def is_enough(sigma, i, j):
+    def stop_condition(sigma, i, j):
         return all(abs(A[i, j]) < s for s in sigma)
 
     def sigmas():
-        return [np.sqrt(max(A[i, i] for i in range(A.shape[0]))) / (10 ** p_i) for p_i in range(p + 1)]
+        return [np.sqrt(max(A[i, i] for i in range(A.shape[0]))) / (10 ** k) for k in range(1, p + 1)]
 
     n = A.shape[0]
     C = np.zeros_like(A)
     i, j = find_max()
     sigma = sigmas()
+    iterat = 0
 
-    while not is_enough(sigma, i, j):
+    while not stop_condition(sigma, i, j):
         iteration(C, i, j)
         A = C.copy()
         i, j = find_max()
+        iterat += 1
 
-    return np.sort(np.diag(C))
+    return np.sort(np.diag(A)), iterat
 
 
-def richardson_method(A: np.array, b: np.array, x: np.array, eps: float = 1e-5):
-    lambda_min, lambda_max = rotation_method(A)[0], rotation_method(A)[-1]
+def richardson_method(A: np.array, b: np.array, x: np.array, eps: float = 1e-10, max_iter=100, n=20):
+    lambdas, i = rotation_method(A)
+    lambda_min, lambda_max = lambdas[0], lambdas[-1]
     tau_0 = 2 / (lambda_min + lambda_max)
     eta = lambda_min / lambda_max
     rho_0 = (1 - eta) / (1 + eta)
 
-    n = 20
     iterations = 0
 
-    while np.linalg.norm(A @ x - b) > eps:
+    for iteration in range(max_iter):
+        if np.linalg.norm(A @ x - b) <= eps:
+            break
         for k in range(1, n + 1):
             v_k = np.cos((2 * k - 1) * np.pi / (2 * n))
             t_k = tau_0 / (1 + rho_0 * v_k)
@@ -103,38 +110,68 @@ def check_solution(A: np.array, b: np.array, x: np.array):
 
 def check_eigenvalues(A: np.array):
     # Вычисление собственных значений через метод вращения
-    computed_eigenvalues = rotation_method(A)
+    computed_eigenvalues, i = rotation_method(A)
 
     # Вычисление собственных значений через встроенный метод
     true_eigenvalues = np.linalg.eigvals(A)
 
+    print("Выполнен поиск собственных значений. Потребовалось итераций: ", i)
     print("Истинные собственные значения:", np.sort(true_eigenvalues))
     print("Собственные значения, полученные методом вращения:", computed_eigenvalues)
 
     # Проверка характеристического уравнения
     dets = [np.linalg.det(A - eigenval * np.eye(A.shape[0])) for eigenval in computed_eigenvalues]
     print("Определители для det(A - I*lambda):", dets)
-    print("Все определители близки к нулю:", np.allclose(dets, 0, atol=1e-10))
+    print("Все определители близки к нулю (погрешность 10го порядка):", np.allclose(dets, 0, atol=1e-10))
     print()
 
 
 def main():
-    A = np.array([[1.00, 0.42, 0.54, 0.66],
-                  [0.42, 1.00, 0.32, 0.44],
-                  [0.54, 0.32, 1.00, 0.22],
-                  [0.66, 0.44, 0.22, 1.00]])
-    b = np.array([1, 1, 1, 1])
-    x = np.zeros_like(b)
+    matrices = {
+        "Матрица 1": (np.array([[-0.168700, 0.353699, 0.008540, 0.733624],
+                                [0.353699, 0.056519, -0.723182, -0.076440],
+                                [0.008540, -0.723182, 0.015938, 0.342333],
+                                [0.733624, -0.076440, 0.342333, -0.045744]]),
+                      np.array([-0.943568, -0.744036, 0.687843, 0.857774])),
+        "Матрица 2": (np.array([[2.2, 1, 0.5, 2],
+                                [1, 1.3, 2, 1],
+                                [0.5, 2, 0.5, 1.6],
+                                [2, 1, 1.6, 2]]),
+                      np.array([5.652, 1.545, -1.420, 0.2226])),
+        "Матрица 3": (np.array([[1.00, 0.42, 0.54, 0.66],
+                                [0.42, 1.00, 0.32, 0.44],
+                                [0.54, 0.32, 1.00, 0.22],
+                                [0.66, 0.44, 0.22, 1.00]]),
+                      np.array([2.3227, 0.7967, 0.6383, 0.2423])),
+        "Проверяющая матрица": (np.array(
+            [[2.00, 1.00],
+             [1.00, 2.00]]),
+                                np.array([4, 5])),
+        "Матрица методички": (np.array(
+            [[2, 1, 1],
+             [1, 2.5, 1],
+             [1, 1, 3]
+             ]),
 
-    x, iterations = richardson_method(A, b, x)
+                              np.array([1.185089, 4.555030, 1.759839])),
+    }
 
-    print(f"Итерационный процесс завершился за {iterations} итераций с решением: {x} \n")
+    for name, (A, expected_eigenvalues) in matrices.items():
+        print(f"-------------{name}-------------")
 
-    # Проверка решения
-    check_solution(A, b, x)
+        b = np.array([1 for i in range(expected_eigenvalues.size)])
+        # x = np.array([1 for i in range(expected_eigenvalues.size)])
+        x = np.zeros_like(b)
 
-    # Проверка собственных значений
-    check_eigenvalues(A)
+        x, iterations = richardson_method(A, b, x)
+
+        print(f"Итерационный процесс завершился за {iterations} итераций с решением: {x} \n")
+
+        # Проверка решения
+        check_solution(A, b, x)
+
+        # Проверка собственных значений
+        check_eigenvalues(A)
 
 
 if __name__ == '__main__':
